aae3ad6f0f8315d4db235860de8fc09b4e5ca8ba
PeterLemon <krom@gbadev.org>
2015-11-11 22:35:41 +0000

    APU Twinkle Song Demo

--- APU/Twinkle/Twinkle.asm
+++ APU/Twinkle/Twinkle.asm
@@ -20,52 +20,54 @@ seek($8000); Start:
 
 LoopSong:
   ldx #0 // X = Song Offset
-  LoopNotes:
-    ldy TwinkleCH1,x  // Y = Period Table Offset
-    cpy #REST  // Compare Y T0 REST Character ($FF)
-    beq KeyOFF // IF (Y == REST) GOTO KeyOFF
 
-    // ELSE Key ON
+  APUCHAN1: // APU Channel 1
+    ldy SONGCHAN1,x // Y = Channel 1: Period Table Offset
+    cpy #REST   // Compare Y T0 REST Character ($FF)
+    beq KEYOFF1 // IF (Y == REST) Channel 1: Key OFF
+
+    // ELSE Channel 1: Key ON
     lda PeriodTable,y // A = Channel 1: Frequency Lo
-    sta REG_APUFREQL1 // Store To Channel 1 Frequency Lo ($4002)
+    sta REG_APUFREQL1 // Store Channel 1: Frequency Lo ($4002)
 
     iny // Y++ (Increment Period Table Offset)
     lda PeriodTable,y // A = Channel 1: Frequency Hi (Bits 0..3)
-    sta REG_APUFREQH1 // Store To Channel 1 Frequency Hi ($4003)
+    sta REG_APUFREQH1 // Store Channel 1: Frequency Hi ($4003)
 
     lda #%10111111   // Channel 1: Volume = $F (Bits 0..3), Fixed Volume (Bit 4), Enable Looping (Bit 5), Duty Cycle 50% (Bits 6..7)
-    sta REG_APUCTRL1 // Store To Channel 1 Control ($4000)
-    jmp VSYNCDelay // GOTO VSYNC Delay
+    sta REG_APUCTRL1 // Store Channel 1: Control ($4000)
+    jmp VSYNCDelay   // GOTO VSYNC Delay
 
-    KeyOFF:
+    KEYOFF1: // Channel 1: Key OFF
       lda #%10011111   // Channel 1: Volume = $F (Bits 0..3), Fixed Volume (Bit 4), Disable Looping (Bit 5), Duty Cycle 50% (Bits 6..7)
-      sta REG_APUCTRL1 // Store To Channel 1 Control ($4000)
+      sta REG_APUCTRL1 // Store Channel 1: Control ($4000)
 
-    VSYNCDelay: // 250 MS Delay (15 NTSC VSYNCS)
-      ldy #15 // Y = 15 (VSYNC Count)
-      - // Loop Delay
-        bit REG_PPUSTATUS // Read PPUSTATUS To Reset Address Latch ($2002)
-        bpl - // Wait For VBLANK
-        dey   // Y--
-        bne - // IF (VSYNC Count != 0) Loop Delay
-    
-    inx // X++ (Increment Song Offset)
-    cpx #TwinkleCH1End-TwinkleCH1 // Compare Song Offset To Song Size
-    bne LoopNotes // IF (Song Offset != Song Size) Loop Notes
+  VSYNCDelay: // 250 MS Delay (15 NTSC VSYNCS)
+    ldy #15 // Y = 15 (VSYNC Count)
+    - // Wait For VBLANK
+      bit REG_PPUSTATUS // Read PPUSTATUS To Reset Address Latch ($2002)
+      bpl - // Wait For VBLANK
+      dey   // Y-- (Decrement VSYNC Count)
+      bne - // IF (VSYNC Count != 0) Wait For VBLANK
+  
+  inx // X++ (Increment Song Offset)
+  cpx #SongEnd - SongStart // Compare Song Offset To Song Size
+  bne APUCHAN1 // IF (Song Offset != Song Size) GOTO APU Channel 1
 
   jmp LoopSong // GOTO Loop Song
 
 PeriodTable: // NTSC Period Table Used For APU Note Freqencies
   NTSCPeriodTable() // NTSC Timing, 10 Octaves: C0..B9 (120 Words)
 
-TwinkleCH1: // APU Channel 1 Song Data At 250ms (15 NTSC VSYNCS)
-  db C4, REST, C4, REST, G4, REST, G4, REST, A4, REST, A4, REST, G4, REST, REST, REST // Twinkle Twinkle Little Star...
-  db F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, D4, REST, C4, REST, REST, REST //   How I Wonder What You Are...
-  db G4, REST, G4, REST, F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, REST, REST //  Up Above The World So High...
-  db G4, REST, G4, REST, F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, REST, REST //   Like A Diamond In The Sky...
-  db C4, REST, C4, REST, G4, REST, G4, REST, A4, REST, A4, REST, G4, REST, REST, REST // Twinkle Twinkle Little Star...
-  db F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, D4, REST, C4, REST, REST, REST //   How I Wonder What You Are...
-TwinkleCH1End:
+SongStart: // APU Channel 1 Song Data At 250ms (15 NTSC VSYNCS)
+  SONGCHAN1: // APU Channel 1 Song Data At 250ms (15 NTSC VSYNCS)
+    db C4, REST, C4, REST, G4, REST, G4, REST, A4, REST, A4, REST, G4, REST, REST, REST // 1. Twinkle Twinkle Little Star...
+    db F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, D4, REST, C4, REST, REST, REST // 2.   How I Wonder What You Are...
+    db G4, REST, G4, REST, F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, REST, REST // 3.  Up Above The World So High...
+    db G4, REST, G4, REST, F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, REST, REST // 4.   Like A Diamond In The Sky...
+    db C4, REST, C4, REST, G4, REST, G4, REST, A4, REST, A4, REST, G4, REST, REST, REST // 5. Twinkle Twinkle Little Star...
+    db F4, REST, F4, REST, E4, REST, E4, REST, D4, REST, D4, REST, C4, REST, REST, REST // 6.   How I Wonder What You Are...
+SongEnd:
 
 // CHR BANK 0 (8KB)
 seek($18000); fill $2000 // Fill CHR Bank 0 With Zero Bytes
